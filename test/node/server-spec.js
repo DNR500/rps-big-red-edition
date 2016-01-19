1e8c57c94a3989b8851266cb6681ddd036c57015
Nathan Richards <nathan.richards@bbc.co.uk>
2016-01-19 19:09:11 +0000

    arrows used to replace functions in the node unit tests

--- test/node/server-spec.js
+++ test/node/server-spec.js
@@ -2,18 +2,18 @@ import ioClient from 'socket.io-client';
 import config from '../../src/config';
 import {expect} from 'chai';
 
-describe('RPS socket server', function() {
+describe('RPS socket server', ()=>{
     var server = require('../../src/node/httpapp');
 
     var socket_1;
     var socket_2;
     var socket_3;
 
-    beforeEach(function() {
+    beforeEach(() => {
         server.start(3000);
     });
 
-    afterEach(function() {
+    afterEach(() => {
         if(socket_1){
             socket_1.disconnect();
             socket_1 = null;
@@ -30,10 +30,10 @@ describe('RPS socket server', function() {
         config.timeoutDuration = 20000;
     });
 
-    describe('initial connection', function() {
-        it('should recognise client connections with the connect event', function(done) {
+    describe('initial connection', ()=>{
+        it('should recognise client connections with the connect event', done=>{
             var count = 0;
-            function checkAll3Connected() {
+            var checkAll3Connected = ()=>{
                 count++;
                 if(count>=3){
                     done();
@@ -46,24 +46,24 @@ describe('RPS socket server', function() {
             socket_2.once('connect', checkAll3Connected);
             socket_3.once('connect', checkAll3Connected);
         });
-        it('should disconnect any socket that does not send a name', function(done) {
+        it('should disconnect any socket that does not send a name', done => {
             var connected = false;
 
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true });
-            socket_1.once('connect', function(){
+            socket_1.once('connect', ()=>{
                 connected = true;
             });
-            socket_1.once('disconnect', function(){
+            socket_1.once('disconnect', ()=>{
                 if(connected){
                     done();
                 }
             });
 
         });
-        it('should allow names to be sent between paired connections using the receive-enemy-name event', function(done) {
+        it('should allow names to be sent between paired connections using the receive-enemy-name event', done=>{
             var count = 0;
 
-            function checkNames() {
+            var checkNames = ()=>{
                 count++;
                 if(count >= 2){
                     expect(socket_2.enemyName).to.equal('firstSocket');
@@ -72,31 +72,31 @@ describe('RPS socket server', function() {
                 }
             }
 
-            function createSocket2() {
+            var  createSocket2 = ()=>{
                 socket_2 = ioClient.connect('http://localhost:3000', {'forceNew': true, query:'displayname=secondSocket' });
-                socket_2.once('receive-enemy-name', function(data) {
+                socket_2.once('receive-enemy-name', data=>{
                     socket_2.enemyName = data.displayName;
                     checkNames();
                 });
             }
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('connect', function() {
+            socket_1.once('connect', ()=>{
                 createSocket2();
             });
-            socket_1.once('receive-enemy-name', function(data) {
+            socket_1.once('receive-enemy-name', data=>{
                 socket_1.enemyName = data.displayName;
                 checkNames();
             });
         });
-        it('should allow weapons to be sent between paired connections using the weapon-choice event', function(done) {
+        it('should allow weapons to be sent between paired connections using the weapon-choice event', done=>{
             var count = 0;
 
-            function emitWeapons() {
+            var emitWeapons = ()=>{
                 socket_1.emit('weapon-selected', {weaponChoice:'scissors'});
                 socket_2.emit('weapon-selected', {weaponChoice:'rock'});
             }
 
-            function checkChoices() {
+            var checkChoices = ()=>{
                 count++;
                 if(count >= 2){
                     expect(socket_2.enemyWeapon).to.equal('scissors');
@@ -105,102 +105,102 @@ describe('RPS socket server', function() {
                 }
             }
 
-            function createSocket2() {
+            var createSocket2 = ()=>{
                 socket_2 = ioClient.connect('http://localhost:3000', {'forceNew': true, query:'displayname=secondSocket' });
-                socket_2.once('connect', function() {
+                socket_2.once('connect', ()=>{
                     emitWeapons();
                 });
-                socket_2.once('receive-enemy-weapon', function(data) {
+                socket_2.once('receive-enemy-weapon', data=>{
                     socket_2.enemyWeapon = data.weaponChoice;
                     checkChoices();
                 });
             }
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('connect', function() {
+            socket_1.once('connect', ()=>{
                 createSocket2();
             });
-            socket_1.once('receive-enemy-weapon', function(data) {
+            socket_1.once('receive-enemy-weapon', data=>{
                 socket_1.enemyWeapon = data.weaponChoice;
                 checkChoices();
             });
         });
-        it('should not allow unrecognised weapons to be sent', function(done) {
+        it('should not allow unrecognised weapons to be sent', done=>{
             var count = 0;
 
-            function checkChoices() {
+            var checkChoices = ()=>{
                 count++;
                 expect(socket_2.enemyWeapon).to.be.undefined;
                 expect(socket_1.enemyWeapon).to.equal('rock');
                 done();
             }
 
-            function emitWeapons() {
+            var emitWeapons = ()=>{
                 socket_1.emit('weapon-selected', {weaponChoice:'asdf'});
                 socket_2.emit('weapon-selected', {weaponChoice:'rock'});
                 setTimeout(checkChoices, 1000);
             }
 
-            function createSocket2() {
+            var createSocket2 = ()=>{
                 socket_2 = ioClient.connect('http://localhost:3000', {'forceNew': true, query:'displayname=secondSocket' });
-                socket_2.once('connect', function() {
+                socket_2.once('connect', ()=>{
                     emitWeapons();
                 });
-                socket_2.once('receive-enemy-weapon', function(data) {
+                socket_2.once('receive-enemy-weapon', data=>{
                     socket_2.enemyWeapon = data.weaponChoice;
                 });
             }
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('connect', function() {
+            socket_1.once('connect', ()=>{
                 createSocket2();
             });
-            socket_1.once('receive-enemy-weapon', function(data) {
+            socket_1.once('receive-enemy-weapon', data=>{
                 socket_1.enemyWeapon = data.weaponChoice;
             });
         });
-        it('should allow disconnection to be relayed between paired connections', function(done) {
+        it('should allow disconnection to be relayed between paired connections', done=>{
             var count = 0;
 
-            function callDisconnect() {
+            var callDisconnect = ()=>{
                 socket_1.disconnect();
             }
-            function checkDisconnect() {
+            var checkDisconnect = ()=>{
                 count++;
                 if(count >= 2){
                     done();
                 }
             }
 
-            function createSocket2() {
+            var createSocket2 = ()=>{
                 socket_2 = ioClient.connect('http://localhost:3000', {'forceNew': true, query:'displayname=secondSocket' });
-                socket_2.once('connect', function() {
+                socket_2.once('connect', ()=>{
                     callDisconnect();
                 });
-                socket_2.once('disconnect', function() {
+                socket_2.once('disconnect', ()=>{
                     checkDisconnect();
                 });
             }
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('connect', function() {
+            socket_1.once('connect', ()=>{
                 createSocket2();
             });
-            socket_1.once('disconnect', function() {
+            socket_1.once('disconnect', ()=>{
                 checkDisconnect();
             });
         });
 
-        it('should signal disconnect due to timeout if connection is not paired within specified timeout', function(done) {
+        it('should signal disconnect due to timeout if connection is not paired within specified timeout', done=>{
             server.stop();
 
             config.timeoutDuration = 1000;
             server.start(3000);
 
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('disconnect', function() {
+            socket_1.once('disconnect', ()=>{
                 done();
             });
         });
 
-        it('should signal disconnect due to timeout if pairs do not disconnect after timeout duration', function(done) {
+        it('should signal disconnect due to timeout if pairs do not disconnect after timeout duration', done=>{
             server.stop();
 
             config.timeoutDuration = 1000;
@@ -208,24 +208,24 @@ describe('RPS socket server', function() {
 
             var count = 0;
 
-            function checkDisconnect() {
+            var checkDisconnect = ()=>{
                 count++;
                 if(count >= 2){
                     done();
                 }
             }
 
-            function createSocket2() {
+            var createSocket2 = ()=>{
                 socket_2 = ioClient.connect('http://localhost:3000', {'forceNew': true, query:'displayname=secondSocket' });
-                socket_2.once('disconnect', function() {
+                socket_2.once('disconnect', ()=>{
                     checkDisconnect();
                 });
             }
             socket_1 = ioClient.connect('http://localhost:3000', {'forceNew': true , query:'displayname=firstSocket'});
-            socket_1.once('connect', function() {
+            socket_1.once('connect', ()=>{
                 createSocket2();
             });
-            socket_1.once('disconnect', function() {
+            socket_1.once('disconnect', ()=>{
                 checkDisconnect();
             });
         });
